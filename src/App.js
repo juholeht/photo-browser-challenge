7e9e985dac2e5cac8d5099476725689f42e2d87f
Juho Lehtonen <lehtonenjuho@gmail.com>
2023-01-26 19:11:59 +0200

    Still very raw draft of App level routes using Material UI components.

--- src/App.js
+++ src/App.js
@@ -5,26 +5,67 @@ import PhotosListView from './PhotosListView';
 import AlbumsView from './AlbumsView';
 import PageNotFound from './PageNotFound';
 
-function App() {
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const drawerWidth = 240;
+
+const App = () => {
   return (
     <div className="App">
       <Router>
-        <SidebarMenu />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={(
-              <PhotosListView />
-            )}
-          />
-          <Route
-            exact
-            path="/albums"
-            element={(<AlbumsView />)}
-          />
-          <Route path="*" element={(<PageNotFound />)} />
-        </Routes>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+          >
+            <Toolbar>
+              <Typography variant="h6" noWrap component="div">
+                Photo browser
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+              },
+            }}
+            variant="permanent"
+            anchor="left"
+          >
+            <SidebarMenu />
+          </Drawer>
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+          >
+            <Toolbar />
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={(
+                  <PhotosListView />
+                )}
+              />
+              <Route
+                exact
+                path="/albums"
+                element={(<AlbumsView />)}
+              />
+              <Route path="*" element={(<PageNotFound />)} />
+            </Routes>
+          </Box>
+        </Box>
       </Router>
     </div>
   );
